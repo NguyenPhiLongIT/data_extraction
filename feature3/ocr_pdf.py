b0ec857d66b92a2c835202bfbe25dca316255fd9
Phi Long <nguyenphilong.it.123@gmail.com>
2025-01-10 15:57:47 +0700

    fix path tesseract

--- feature3/ocr_pdf.py
+++ feature3/ocr_pdf.py
@@ -1,27 +1,27 @@
 import subprocess
 import os
 
-def ocr(file_path, save_path):
-    base_dir = os.path.abspath(os.path.dirname(__file__))
-    tesseract_exe = os.path.join(base_dir, "..", "lib", "tesseract.exe")
-    
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"File input '{file_path}' không tồn tại.")
-    
-    output_dir = os.path.dirname(save_path)
-    if output_dir and not os.path.exists(output_dir):
-        os.makedirs(output_dir, exist_ok=True)
+TESSERACT_PATH = os.path.abspath("lib/tesseract.exe")
+TESSDATA_PREFIX = os.path.abspath("lib/tessdata")
+GHOSTSCRIPT_PATH = os.path.abspath("./lib/gs10.04.0/bin/gswin64c.exe")
 
-    command = [
-        tesseract_exe, 
-        file_path, 
-        save_path,  
-        "-l", "vie"  
-    ]
-    
+def ocr_pdf(input_pdf, output_pdf, language="vie"):     #language="vie"/language="eng"
     try:
-        subprocess.run(command, check=True)
-        print(f"OCR thành công. File đã lưu tại: {save_path}")
+        env = os.environ.copy()
+        env["TESSDATA_PREFIX"] = TESSDATA_PREFIX
+        env["PATH"] = os.pathsep.join([
+            os.path.dirname(TESSERACT_PATH),
+            os.path.dirname(GHOSTSCRIPT_PATH),
+            env.get("PATH", "")
+        ])
+        subprocess.run([
+            "ocrmypdf", 
+            "--language", language,
+            "--tesseract-config", "pdf", 
+            input_pdf, 
+            output_pdf
+        ], check=True, env=env)
+        print(f"OCR hoàn tất. File đã lưu tại: {output_pdf}")
     except subprocess.CalledProcessError as e:
         print(f"Lỗi khi thực hiện OCR: {e}")
 
@@ -36,17 +36,16 @@ def ocr_pdf_directory(input_dir, output_dir):
         file_path = os.path.join(input_dir, filename)
         
         if os.path.isfile(file_path) and filename.endswith('.pdf'):
-            output_file_path = os.path.join(output_dir, filename.replace('.pdf', '_ocr.pdf')) 
-
+            output_file_path = os.path.join(output_dir, filename) 
             try:
                 print(f"Đang thực hiện OCR cho tệp: {filename}")
-                print(f"Đường dẫn kết quả: {output_file_path}")  
-                ocr(file_path, output_file_path)
+                ocr_pdf(file_path, output_file_path)
                 print(f"OCR hoàn thành cho tệp: {filename}")
             except Exception as e:
                 print(f"Lỗi khi thực hiện OCR cho tệp {filename}: {e}")
 
 if __name__ == "__main__":
-    input_dir = "../file"  
-    output_dir = "../result"  
+    input_dir = "file"  
+    output_dir = "result"  
+
     ocr_pdf_directory(input_dir, output_dir)
